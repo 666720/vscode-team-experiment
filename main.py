da5aaccee21df308861289fb834569cf80113fd4
wjy <2240372338@qq.com>
2025-04-09 13:15:25 +0800

    git add main.py git commit -m "解决冲突后的版本" git push

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def greet(name):
 print(f"Hello, {name}!")
 def goodbye(name):
 print(f"Goodbye, {name}!")
-greet("Everyone")
+
 goodbye("GitHub")
-# 
+
+# 修改后的内容：
+greet("GitHub and VSCode, Everyone")
